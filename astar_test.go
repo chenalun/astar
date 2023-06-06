273af042a6b261d1496caf048e2f08b6ccd5a04c
chenalun <2850592953@qq.com>
2023-06-06 20:04:37 +0800

    astar_test

--- astar_test.go
+++ astar_test.go
@@ -6,27 +6,8 @@ import (
 	"testing"
 )
 
-// Test2FindPath 四边形二维寻路
+// Test2FindPath 四边形网格寻路
 func Test2FindPath(t *testing.T) {
-	var list []*astar.Coordinate
-
-	// 障碍物
-	list = append(list,
-		astar.NewCoordinate(1, 2),
-		astar.NewCoordinate(2, 1),
-		astar.NewCoordinate(3, 0),
-	)
-
-	r := astar.NewAstar(astar.Model3, 7, 7).SetObstacle(list...).FindPath(astar.NewCoordinate(0, 1), astar.NewCoordinate(5, 2))
-
-	for _, v := range r {
-		fmt.Println(fmt.Sprintf("最终路线的列表:(x=%d,y=%d,z=%d)", v.X(), v.Y(), v.Z()))
-	}
-
-}
-
-// Test3FindPath 六边形三维寻路
-func Test3FindPath(t *testing.T) {
 	var list []*astar.Coordinate
 
 	list = append(list,
@@ -43,4 +24,23 @@ func Test3FindPath(t *testing.T) {
 	for _, v := range r {
 		fmt.Println(fmt.Sprintf("最终路线的列表:(x=%d,y=%d)", v.X(), v.Y()))
 	}
+}
+
+// Test3FindPath 六边形网格寻路
+func Test3FindPath(t *testing.T) {
+	var list []*astar.Coordinate
+
+	// 障碍物
+	list = append(list,
+		astar.NewCoordinate(1, 2),
+		astar.NewCoordinate(2, 1),
+		astar.NewCoordinate(3, 0),
+	)
+
+	r := astar.NewAstar(astar.Model3, 7, 7).SetObstacle(list...).FindPath(astar.NewCoordinate(0, 1), astar.NewCoordinate(5, 2))
+
+	for _, v := range r {
+		fmt.Println(fmt.Sprintf("最终路线的列表:(x=%d,y=%d,z=%d)", v.X(), v.Y(), v.Z()))
+	}
+
 }
